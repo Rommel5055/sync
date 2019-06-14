ec511001aa3001ae70b6c54e0b6d8fd8ac80996e
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2019-06-14 10:03:41 -0400

    Something something

--- query.php
+++ query.php
@@ -18,11 +18,11 @@ $query = "SELECT c.id,
         c.fullname,
         c.shortname
         FROM mdl_course AS c
-        LEFT JOIN mdl_context AS ct ON c.id = ct.instanceid
-        LEFT JOIN mdl_role_assignments AS ra ON ra.contextid = ct.id
+        INNER JOIN mdl_context AS ct ON c.id = ct.instanceid
+        INNER JOIN mdl_role_assignments AS ra ON ra.contextid = ct.id
         LEFT JOIN mdl_user AS u ON u.id = ra.userid
         LEFT JOIN mdl_role AS r ON r.id = ra.roleid
-        WHERE c.id > 0 AND r.archetype = ?
+        WHERE c.id > 0 
         Group By c.id, c.fullname, c.shortname
         Order By count(u.id), c.id";
 $results = $DB->get_records_sql($query, $par);
