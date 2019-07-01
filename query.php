9aabdfbf827407c986c038f0fc7999e47a852122
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2019-06-30 20:35:43 -0400

    What if?

--- query.php
+++ query.php
@@ -25,7 +25,13 @@ $sql = "SELECT sc.catid,
         WHERE c.id > 0 AND c.enddate >= ?
         Group By c.id, c.fullname, c.shortname
         Order By count(u.id), c.id";
-$results = $DB->get_records_sql($sql);
+$results = $DB->get_records_sql("SELECT sc.catid,
+        count(sc.id) AS ncourses
+        FROM {sync_course} AS sc
+        INNER JOIN {course} as c ON (sc.id = c.id)
+        WHERE c.id > 0 AND c.enddate >= ?
+        Group By c.id, c.fullname, c.shortname
+        Order By count(u.id), c.id");
 
 
 //var_dump($results);
