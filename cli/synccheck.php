20c57b08948043baaa39e770eee9ac7ee5513d16
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2019-06-30 22:03:22 -0400

    var_dump($mails);

--- cli/synccheck.php
+++ cli/synccheck.php
@@ -59,6 +59,7 @@ cli_heading('Check'); // TODO: localize
 echo "\nStarting at ".date("F j, Y, G:i:s")."\n";
 
 $mails = explode("," ,$CFG->sync_mailalert);
+var_dump($mails);
 $userlist = array();
 foreach ($mails as $mail){
     $usercfg = $DB->get_record('user', array('email' => $mail));
